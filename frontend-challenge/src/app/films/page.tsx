466c05b9db5fdf4d49fdb5a0bc0c8fbf15e510fa
Bruno Burin <cburin.bruno@gmail.com>
2024-11-24 00:53:11 -0300

    create pages for films and planets

---
+++ frontend-challenge/src/app/films/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Table, { ColumnConfig } from "../components/Table";
+
+import { Film } from "@/app/api/entities/films";
+import React from "react";
+import { getAllFilms } from "@/app/api/entities/films";
+import { usePaginatedData } from "../hooks/usePaginatedData";
+
+const FilmPage = () => {
+    const { data: films, currentPage, totalPages, loading, error, setCurrentPage } = usePaginatedData<Film>(
+        () => getAllFilms()
+    );
+
+    const columns: ColumnConfig<Film>[] = [
+        { label: "Title", field: "title", width: "15%" },
+        { label: "Episode", field: "episodeId", width: "5%" },
+        { label: "Opening Crawl", field: "openingCrawl", width: "50%", render: (value) => `${value.toString().slice(0, 240)}...` },
+        { label: "Director", field: "director", width: "10%" },
+        { label: "Producers", field: "producer", width: "15%", render: (value) => Array.isArray(value) ? value.join(", ") : value },
+        { label: "Release Date", field: "releaseDate", width: "5%", render: (value) => new Date(value.toString()).toLocaleDateString() }
+    ];
+
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-4 text-gray-800">Films</h1>
+            {films && (
+                <Table
+                    columns={columns}
+                    rows={films}
+                    totalPages={totalPages}
+                    currentPage={currentPage}
+                    onPageChange={setCurrentPage}
+                    loading={loading}
+                />
+            )}
+        </div>
+    );
+};
+
+export default FilmPage;
---
+++ frontend-challenge/src/app/planets/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Table, { ColumnConfig } from "../components/Table";
+
+import { Planet } from "@/app/api/entities/planets";
+import React from "react";
+import { getAllPlanets } from "@/app/api/entities/planets";
+import { usePaginatedData } from "../hooks/usePaginatedData";
+
+const PlanetPage = () => {
+    const { data: planets, currentPage, totalPages, loading, error, setCurrentPage } = usePaginatedData<Planet>(
+        () => getAllPlanets()
+    );
+
+    const columns: ColumnConfig<Planet>[] = [
+        { label: "Name", field: "name", width: "20%" },
+        { label: "Climate", field: "climate", width: "15%" },
+        { label: "Terrain", field: "terrain", width: "15%", render: (value) => Array.isArray(value) ? value.join(", ") : value },
+        { label: "Diameter", field: "diameter", width: "10%", render: (value) => `${value} km` },
+        { label: "Population", field: "population", width: "15%", render: (value) => value || "Unknown" },
+        { label: "Gravity", field: "gravity", width: "10%" },
+        { label: "Rotation Period", field: "rotationPeriod", width: "7%", render: (value) => `${value} hours` },
+        { label: "Orbital Period", field: "orbitalPeriod", width: "8%", render: (value) => `${value} days` },
+        { label: "Surface Water", field: "surfaceWater", width: "10%", render: (value) => `${value}%` },
+    ];
+
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-4 text-gray-800">Planets</h1>
+            {planets && (
+                <Table
+                    columns={columns}
+                    rows={planets}
+                    totalPages={totalPages}
+                    currentPage={currentPage}
+                    onPageChange={setCurrentPage}
+                    loading={loading}
+                />
+            )}
+        </div>
+    );
+};
+
+export default PlanetPage;
